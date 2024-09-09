7294e9b36d29e204baf774f152fb51892858c7f3
ThinLiquid <thnlqd@gmail.com>
2024-09-09 06:46:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 
+const jsToBottomNoModule = () => {
+  return {
+    name: "no-attribute",
+    transformIndexHtml(html) {
+      html = html.replace(`type="module" crossorigin`, "")
+      let scriptTag = html.match(/<script[^>]*>(.*?)<\/script[^>]*>/)[0]
+      console.log("\n SCRIPT TAG", scriptTag, "\n")
+      html = html.replace(scriptTag, "")
+      html = html.replace("<!-- # INSERT SCRIPT HERE -->", scriptTag)
+      return html;
+    }
+  }
+}
+
 export default defineConfig({
   build: {
-    target: "ES2022" // <--------- ✅✅✅✅✅✅
+    target: "ES2022"
   },
+  plugins: [jsToBottomNoModule()],
 })
+
